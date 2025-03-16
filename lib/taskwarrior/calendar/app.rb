ae2c9ff7cf60ff13b3bd36491609c6bf1b95ebcc
tschaefer <github@blackox.org>
2025-03-16 08:34:01 +0100

    feat(app): set default command 'convert'

--- lib/taskwarrior/calendar/app.rb
+++ lib/taskwarrior/calendar/app.rb
@@ -7,6 +7,8 @@ class Taskwarrior
   class Calendar
     module App
       class Command < Taskwarrior::Calendar::App::BaseCommand
+        self.default_subcommand = 'convert'
+
         subcommand 'convert', 'Convert tasks to events', Taskwarrior::Calendar::App::ConvertCommand
       end
     end
